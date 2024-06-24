ed5a729ef907fa84ad74f1c1042d8855f53f42d6
Lucas Kalks <lucaskalks00@gmail.com>
2024-06-24 10:40:48 -0300

    informações de conexão ao banco em variáveis

--- conexao.php
+++ conexao.php
@@ -4,10 +4,15 @@ session_start();
 
 global $conectar;
 
+$dsn = 'mysql:host=localhost;dbname=controle_locacao;charset=utf8mb4';
+$username = "root";
+$password = "root";
+$options = [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4'];
+
 
 try {
   //Conecta ao banco de dados
-  $conectar = new PDO("mysql:host=localhost;dbname=controle_locacao", "root", "root");
+  $conectar = new PDO($dsn, $username, $password, $options);
   $conectar->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
