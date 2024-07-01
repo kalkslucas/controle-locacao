56d3a62c0086d6cc8c7f3883113b8da1a19eead6
Lucas Kalks <lucaskalks00@gmail.com>
2024-07-01 13:39:23 -0300

    buscaCep.js incluido em editar locador

--- form-editar-locador.php
+++ form-editar-locador.php
@@ -15,7 +15,7 @@ if(isset($_SESSION['idusuario']) && !empty( $_SESSION['idusuario'] )):
   <link rel="stylesheet" href="assets/css/btn-custom.css">
   <link rel="stylesheet" href="assets/css/ver-locador.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+  <script src="assets/js/buscaCep.js"></script>
 </head>
 <?php
 include_once "conexao.php";
