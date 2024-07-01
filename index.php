25835601e3f34191ae9fd6892fc81f476dcd7962
Lucas Kalks <lucaskalks00@gmail.com>
2024-07-01 09:34:28 -0300

    navbar tamanho logo aumentada

--- index.php
+++ index.php
@@ -16,9 +16,9 @@ if(isset($_SESSION['idusuario']) && !empty( $_SESSION['idusuario'] )):
 
 </head>
 <body class="page">
-<nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
     <div class="container-fluid">
-      <a class="navbar-brand" href="./index.php"><img src="./assets/img/navbar-logo.png" alt=""></a>
+      <a class="navbar-brand" href="./index.php"><img src="./assets/img/navbar-logo.png" alt="logo ultra"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
